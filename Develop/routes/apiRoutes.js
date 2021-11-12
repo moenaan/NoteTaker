3f616431107d6384647abd17d3b6f0771c871f99
Mohammed Samod <mohammedlee27@gmail.com>
2021-11-12 12:42:14 -0600

    updated read me

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -18,12 +18,4 @@ router.post('/notes', (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
-router.delete('/notes/:id', (req, res) => {
-  store
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
-});
-
 module.exports = router;
